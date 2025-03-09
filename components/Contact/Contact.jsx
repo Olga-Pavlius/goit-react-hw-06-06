646501e7260161cb5f28225bf1a7e52874418710
Olga Pavlius <olya.melnyk.9412@gmail.com>
2025-03-09 22:55:59 +0200

    changed li, add <br/.

--- components/Contact/Contact.jsx
+++ components/Contact/Contact.jsx
@@ -3,7 +3,7 @@ import css from "./Contact.module.css"
 export default function Contact({ id, name, number, onDeleteContact }) {
     return (
         <li className={css.item}>
-            <p className={css.text}>{name}: {number}</p>
+            <p className={css.text}>{name} <br /> {number}</p>
             <button className={css.button} onClick={() => onDeleteContact(id)}>
                 Delete
             </button>
